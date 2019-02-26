d440103e5d1fa8c8535c06b88e50f2fb3c513d2d
Jean-Charles <jeancharles.roger@me.com>
2019-02-26 16:12:24 +0100

    Adds a stability test (doesn't pass yet)

--- src/commonTest/kotlin/com/centyllion/common/PerformanceTest.kt
+++ src/commonTest/kotlin/com/centyllion/common/PerformanceTest.kt
@@ -1,40 +1,8 @@
 package com.centyllion.common
 
-import kotlin.random.Random
 import kotlin.test.Test
 
 class PerformanceTest {
-
-    fun dendriteModel(width: Int = 100, height: Int = 100): Model {
-        val ms = Grain(0, "ms", "blue")
-        val mc = Grain(1, "mc", "red", movementProbability = 0.0)
-
-        val r1 = Behaviour(
-            "crystalisation", "", 1.0,
-            mainReaction = Reaction(mc.id, mc.id), reaction = listOf(Reaction(ms.id, mc.id))
-        )
-
-        return Model(
-            "m1", width, height, 1, "test model",
-            listOf(ms, mc), listOf(r1)
-        )
-    }
-
-    fun dendriteSimulation(width: Int = 100, height: Int = 100): Simulation {
-        return Simulation(dendriteModel(width, height)).apply {
-            for (i in 0 until model.dataSize) {
-                val p = Random.nextDouble()
-                when {
-                    p < 0.15 -> addGrainAtIndex(i, this.model.grains[0])
-                }
-            }
-
-            for (i in -2 until 2) {
-                addGrainAtIndex(model.dataSize/2 + i*167, this.model.grains[1])
-            }
-        }
-    }
-
 
     @Test
     fun testDendritePerformance() {
---
+++ src/commonTest/kotlin/com/centyllion/common/StabilityTest.kt
@@ -0,0 +1,23 @@
+package com.centyllion.common
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StabilityTest {
+
+    @Test
+    fun carTest() {
+        val simulation = carSimulation(10, 10)
+        val simulator = Simulator(simulation)
+        val frontGrain = simulation.model.grains[1]
+        val backGrain = simulation.model.grains[2]
+        val count = simulator.lastGrainsCount()
+        val frontCount = count[frontGrain]
+        val backCount = count[backGrain]
+
+        repeat(500) { simulator.oneStep() }
+        val lastCount = simulator.lastGrainsCount()
+        assertEquals(frontCount, lastCount[frontGrain])
+        assertEquals(backCount, lastCount[backGrain])
+    }
+}

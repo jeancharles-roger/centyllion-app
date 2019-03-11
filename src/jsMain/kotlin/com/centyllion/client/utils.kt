044408f1b5ffd0fbba7545012ee3f193b47c6846
Jean-Charles <jeancharles.roger@me.com>
2019-03-11 21:58:17 +0100

    Finalizes refactoring

--- src/jsMain/kotlin/com/centyllion/client/utils.kt
+++ src/jsMain/kotlin/com/centyllion/client/utils.kt
@@ -1,66 +1,9 @@
 package com.centyllion.client
 
-import Keycloak
-import KeycloakInitOptions
-import KeycloakInstance
-import com.centyllion.common.betaRole
-import com.centyllion.common.centyllionHost
 import kotlinx.html.*
-import org.w3c.dom.HTMLAnchorElement
 import org.w3c.dom.HTMLDivElement
 import org.w3c.dom.HTMLElement
-import org.w3c.dom.get
 import kotlin.browser.document
-import kotlin.browser.window
-import kotlin.js.Promise
-
-fun activateNavBar() {
-    val all = document.querySelectorAll(".navbar-burger")
-    for (i in 0 until all.length) {
-        val burger = all[i] as HTMLElement
-        burger.addEventListener("click", { _ ->
-            // Get the target from the "data-target" attribute
-            val target = burger.dataset["target"]
-            if (target != null) {
-                val targetElement = document.getElementById(target) as HTMLElement
-                burger.classList.toggle("is-active")
-                targetElement.classList.toggle("is-active")
-            }
-        })
-    }
-}
-
-fun authenticate(required: Boolean): Promise<KeycloakInstance?> {
-    val keycloak = Keycloak()
-    val options = KeycloakInitOptions(checkLoginIframe = false, promiseType = "native")
-    options.onLoad = if (required) "login-required" else "check-sso"
-    val promise = keycloak.init(options)
-    return promise.then(onFulfilled = { keycloak }, onRejected = { null })
-}
-
-fun initialize(vararg roles: String): Promise<KeycloakInstance?> {
-    activateNavBar()
-    showVersion()
-
-    val isCentyllionHost = window.location.host == centyllionHost
-    val requiredRoles = if (!isCentyllionHost) roles + betaRole else roles
-
-    return authenticate(requiredRoles.isNotEmpty()).then { keycloak ->
-        if (keycloak != null) {
-            if (keycloak.tokenParsed != null) {
-                val userName = document.querySelector("a.cent-user") as HTMLAnchorElement?
-                userName?.innerText = keycloak.tokenParsed.asDynamic().name as String
-                userName?.href = keycloak.createAccountUrl()
-            }
-            val granted = keycloak.authenticated &&
-                    requiredRoles.fold(true) { a, r -> a && keycloak.hasRealmRole(r) }
-
-            if (granted) keycloak else null
-        } else {
-            null
-        }
-    }
-}
 
 data class ColumnSize(
     val desktop: Int, val tablet: Int = desktop, val mobile: Int? = null, val centered: Boolean = false

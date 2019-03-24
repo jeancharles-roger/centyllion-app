60ce1cfe8fd890b5acf5fcdef0f457edde0062a0
Jean-Charles <jeancharles.roger@me.com>
2019-03-24 12:31:50 +0100

    Better re-use of controllers

--- src/jsMain/kotlin/bulma/controllers.kt
+++ src/jsMain/kotlin/bulma/controllers.kt
@@ -25,7 +25,7 @@ class ColumnsController<Data>(
         if (old != new) refresh()
     }
 
-    private var controllers: MutableList<Controller<Data, Column>> = mutableListOf()
+    private var controllers: List<Controller<Data, Column>> = listOf()
 
     val dataControllers: List<Controller<Data, Column>> get() = controllers
 
@@ -33,9 +33,15 @@ class ColumnsController<Data>(
         refresh()
     }
 
-    // TODO implements controller re-use
     override fun refresh() {
-        controllers = data.mapIndexed { index, data -> controllerBuilder(index, data) }.toMutableList()
+        // constructs a resized controllers list to match new size
+        val resizedControllers = List(data.size) { controllers.getOrNull(it) }
+        // reconstructs controllers using the controller at the correct place if it's has the same data
+        // or by constructing a new one.
+        // TODO it's not possible to change a controller's place since it's given in the constructor
+        controllers = data.zip(resizedControllers).mapIndexed { i, (d, c) ->
+            if (c == null || c.data != d) controllerBuilder(i, d)  else c
+        }
         container.columns = controllers.map { it.container }
     }
 }

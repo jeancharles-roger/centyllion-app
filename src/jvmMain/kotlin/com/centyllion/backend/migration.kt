a705cbebc52fcabe929106cf9fd3749ec79d0c7b
Jean-Charles <jeancharles.roger@me.com>
2019-04-10 12:03:02 +0200

    Migrate grain model from 0 to 1

--- src/jvmMain/kotlin/com/centyllion/backend/migration.kt
+++ src/jvmMain/kotlin/com/centyllion/backend/migration.kt
@@ -8,25 +8,41 @@ abstract class Migration(val from: Int, val to: Int) {
     abstract fun migrate(document: Document)
 }
 
-
 val migrationGrainModelV0toV1 = object : Migration(0, 1) {
     override fun migrate(document: Document) {
-        document.get("")
+        val model = document["model"]
+        if (model is Document) {
+            val behaviours = model["behaviours"]
+            if (behaviours is List<*>) {
+                behaviours.filterIsInstance<Document>().forEach { behaviour ->
+                    val transform = behaviour.getBoolean("transform") ?: false
+                    behaviour.remove("transform")
+                    behaviour["sourceReactive"] = if (transform) 0 else -1
+
+                    val reactions = behaviour["reaction"]
+                    if (reactions is List<*>) {
+                        reactions.filterIsInstance<Document>().forEach { reaction ->
+                            val reactionTransform = reaction.getBoolean("transform") ?: false
+                            reaction.remove("transform")
+                            reaction["sourceReactive"] = if (reactionTransform) 0 else -1
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 val migrations: Map<KSerializer<*>, List<Migration>> = mapOf(
-    GrainModelDescription.serializer() to listOf()
+    GrainModelDescription.serializer() to listOf(migrationGrainModelV0toV1)
 )
 
-val migrationVersions = migrations
-    .map { it.key to (it.value.map { it.to }.max() ?: 0) }.toMap()
-
+/** Migrates document to current version using the [migrations]. */
 fun <T> migrate(serializer: KSerializer<T>, document: Document): Document {
-    val targetVersion = migrationVersions[serializer] ?: 0
     val currentVersion = document.getInteger("version") ?: 0
-    if (currentVersion < targetVersion) {
-
+    (migrations[serializer] ?: emptyList()).filter { it.from >= currentVersion }.forEach {
+        it.migrate(document)
+        document["version"] = it.to
     }
     return document
 }

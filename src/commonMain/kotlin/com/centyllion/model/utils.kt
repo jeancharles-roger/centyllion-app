f0dcc45ce8a79ccc6716ef06984a13f8ae7fb963
Jean-Charles <jeancharles.roger@me.com>
2019-04-11 09:34:17 +0200

    First diff implementation

--- src/commonMain/kotlin/com/centyllion/model/utils.kt
+++ src/commonMain/kotlin/com/centyllion/model/utils.kt
@@ -9,6 +9,84 @@ internal fun <T> List<List<T>>.combine(prefix: List<T>): List<List<T>> =
         else -> drop(1).let { tail ->
             first().flatMap {
                 tail.combine(prefix + it)
-            } }
+            }
+        }
     }
 
+
+enum class DiffAction { Replaced, Added, Removed }
+
+/** Diff description */
+data class Diff<T>(val action: DiffAction, val index: Int, val element: T)
+
+/** Computes differences between [this] and [other]. It returns a list of [Diff] that describes the changes */
+internal fun <T> List<T>.diff(other: List<T>): List<Diff<T>> {
+    var iSource = 0
+    var iOther = 0
+
+    val result = mutableListOf<Diff<T>>()
+    while (iSource < size || iOther < other.size) {
+        when {
+            iSource >= size -> {
+                // source is empty
+                result.add(Diff(DiffAction.Added, iOther, other[iOther]))
+                iOther += 1
+            }
+            iOther >= other.size -> {
+                // other empty
+                result.add(Diff(DiffAction.Removed, iSource, this[iSource]))
+                iSource += 1
+            }
+            this[iSource] == other[iOther] -> {
+                // nothing changed, next
+                iSource += 1
+                iOther += 1
+            }
+            iSource + 1 == size || iOther + 1 == other.size -> {
+                // last element for source or iOther
+                result.add(Diff(DiffAction.Replaced, iSource, other[iOther]))
+                iSource += 1
+                iOther += 1
+            }
+            this[iSource + 1] == other[iOther] -> {
+                // next source is current other
+                result.add(Diff(DiffAction.Removed, iSource, this[iSource]))
+                iSource += 1
+            }
+            this[iSource] == other[iOther+1] -> {
+                // next other is current source
+                result.add(Diff(DiffAction.Added, iOther, other[iOther]))
+                iOther += 1
+            }
+            else -> {
+                // just different
+                result.add(Diff(DiffAction.Replaced, iSource, other[iOther]))
+                iSource += 1
+                iOther += 1
+            }
+        }
+    }
+
+    return result
+}
+
+
+/** From a list of [Diff] ([diff]) it computes the result one */
+internal fun <T> List<T>.applyDiff(diff: List<Diff<T>>): List<T> {
+    val result = this.toMutableList()
+    var delta = 0
+    diff.forEach {
+        when (it.action) {
+            DiffAction.Added -> {
+                result.add(it.index, it.element)
+                delta -= 1
+            }
+            DiffAction.Removed -> {
+                result.removeAt(it.index - delta)
+                delta += 1
+            }
+            DiffAction.Replaced -> result[it.index - delta] = it.element
+        }
+    }
+    return result
+}
---
+++ src/commonTest/kotlin/com/centyllion/model/DiffText.kt
@@ -0,0 +1,94 @@
+package com.centyllion.model
+
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DiffTest {
+
+    fun <T> testDiff(source: List<T>, other: List<T>, diff: List<Diff<T>>? = null) {
+        val computedDiff = source.diff(other)
+        if (diff != null) assertEquals(diff, computedDiff)
+        assertEquals(other, source.applyDiff(computedDiff))
+    }
+
+    @Test
+    fun testAdd1() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "2", "3", "4", "5")
+    )
+
+    @Test
+    fun testAdd2() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "1bis", "2", "2bis", "3", "4")
+    )
+
+    @Test
+    fun testAdd3() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "2", "2bis", "2ter", "3", "4")
+    )
+
+    @Test
+    fun testAdd4() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "2", "2bis", "3", "3bis", "4", "4bis")
+    )
+
+    @Test
+    fun testAdd5() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "1bis", "1ter", "1qar", "2", "3", "4")
+    )
+
+    @Test
+    fun testRemove1() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "2", "3")
+    )
+
+    @Test
+    fun testRemove2() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("2", "3", "4")
+    )
+
+    @Test
+    fun testRemove3() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "3", "4")
+    )
+
+    @Test
+    fun testRemove4() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("3", "4")
+    )
+
+    @Test
+    fun testRemove5() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "4")
+    )
+
+    @Test
+    fun testChange1() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "2bis", "3", "4")
+    )
+
+    @Test
+    fun testChange2() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1bis", "2bis", "3", "4")
+    )
+
+    @Test
+    fun testChange3() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1", "2bis", "3bis", "4")
+    )
+
+    @Test
+    fun testChange4() = testDiff(
+        listOf("1", "2", "3", "4"), listOf("1bis", "2bis", "3bis", "4bis")
+    )
+
+    fun randomList(size: Int = 20, max: Int = 10) = ArrayList<String>(size).apply {
+        (0 until size).forEach { add("${Random.nextInt(max)}") }
+    }
+
+    @Test
+    fun randomTest() {
+        // Tests diff apply over totally random list
+        (0 until 50).forEach { testDiff(randomList(), randomList()) }
+    }
+}

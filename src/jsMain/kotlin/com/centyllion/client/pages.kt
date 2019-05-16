c52b8795efc0fac835915a5937fda56d46d8c1c6
Jean-Charles <jeancharles.roger@me.com>
2019-05-16 17:07:03 +0200

    Removes explore page

--- src/jsMain/kotlin/com/centyllion/client/pages.kt
+++ src/jsMain/kotlin/com/centyllion/client/pages.kt
@@ -1,17 +1,12 @@
 package com.centyllion.client
 
 import bulma.BulmaElement
-import bulma.Title
-import bulma.div
-import bulma.noContextColumnsController
-import com.centyllion.client.controller.FeaturedController
 import com.centyllion.client.page.AdministrationPage
 import com.centyllion.client.page.HomePage
 import com.centyllion.client.page.ModelPage
 import com.centyllion.client.page.ShowPage
 import com.centyllion.common.adminRole
 import com.centyllion.common.modelRole
-import com.centyllion.model.FeaturedDescription
 import keycloak.KeycloakInstance
 
 data class Page(
@@ -30,7 +25,6 @@ const val contentSelector = "section.cent-main"
 val pages = listOf(
     Page("Home", "home", true, null, true, ::HomePage),
     Page("Model", "model", true, modelRole, true, ::ModelPage),
-    Page("Explore", "explore", false, null, true, ::explore),
     Page("Administration", "administration", true, adminRole, true, ::AdministrationPage),
     Page("Show", "show", false, null, false, ::ShowPage)
 )
@@ -38,21 +32,3 @@ val pages = listOf(
 val mainPage = pages[0]
 
 val showPage = pages.find { it.id == "show" }!!
-
-fun explore(appContext: AppContext): BulmaElement {
-    val featuredController = noContextColumnsController<FeaturedDescription, FeaturedController>(emptyList())
-    { parent, data ->
-        val controller = FeaturedController(data)
-        controller.body.root.onclick = {
-            openPage(showPage, appContext, mapOf("model" to data.modelId, "simulation" to data.simulationId))
-        }
-        controller.body.root.style.cursor = "pointer"
-        controller
-    }
-    val page = div(
-        Title("Explore featured models"), featuredController
-    )
-
-    appContext.api.fetchAllFeatured().then { models -> featuredController.data = models }
-    return page
-}

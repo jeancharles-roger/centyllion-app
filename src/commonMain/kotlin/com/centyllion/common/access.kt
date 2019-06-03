47cab710c4f8036a148b94b0054837096b558752
Jean-Charles <jeancharles.roger@me.com>
2019-06-03 20:39:00 +0200

    Uses creator role

--- src/commonMain/kotlin/com/centyllion/common/access.kt
+++ src/commonMain/kotlin/com/centyllion/common/access.kt
@@ -4,6 +4,5 @@ const val centyllionHost = "centyllion.com"
 const val centyllionBetaHost = "beta.$centyllionHost"
 
 const val adminRole = "admin"
-const val betaRole = "beta"
-const val modelRole = "model"
-const val simulationRole = "simulation"
+// TODO renames to creatorRole
+const val modelRole = "creator"

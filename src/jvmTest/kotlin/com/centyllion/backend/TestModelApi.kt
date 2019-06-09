0061c1c8a955171d97165643f988a5ff0979f76c
Jean-Charles <jeancharles.roger@me.com>
2019-06-09 22:10:55 +0200

    Corrects request model

--- src/jvmTest/kotlin/com/centyllion/backend/TestModelApi.kt
+++ src/jvmTest/kotlin/com/centyllion/backend/TestModelApi.kt
@@ -1,8 +1,8 @@
 package com.centyllion.backend
 
 import com.centyllion.model.GrainModelDescription
+import com.centyllion.model.ResultPage
 import io.ktor.util.KtorExperimentalAPI
-import kotlinx.serialization.list
 import kotlin.test.Test
 
 @KtorExperimentalAPI
@@ -10,7 +10,10 @@ class TestModelApi {
 
     @Test
     fun testRequestModels() = withCentyllion {
-        testGet("/api/model", emptyList(), GrainModelDescription.serializer().list)
+        testGet(
+            "/api/model", ResultPage(emptyList(), 0, 0),
+            ResultPage.serializer(GrainModelDescription.serializer())
+        )
     }
 
 }

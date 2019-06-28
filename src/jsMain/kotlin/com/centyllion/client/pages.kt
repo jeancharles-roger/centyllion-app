9d47b661ba3da5623e11dc87849ed96c7804f2ca
Jean-Charles <jeancharles.roger@me.com>
2019-06-28 18:01:26 +0200

    Corrects import error

--- src/jsMain/kotlin/com/centyllion/client/pages.kt
+++ src/jsMain/kotlin/com/centyllion/client/pages.kt
@@ -6,7 +6,6 @@ import com.centyllion.client.page.ExplorePage
 import com.centyllion.client.page.HomePage
 import com.centyllion.client.page.ShowPage
 import com.centyllion.client.page.SubscriptionPage
-import com.centyllion.client.page.TestPage
 import com.centyllion.common.adminRole
 import keycloak.KeycloakInstance
 import kotlin.js.Promise
@@ -29,8 +28,7 @@ val homePage = Page("Home", "home", true, null, true, ::HomePage)
 val explorePage = Page("Explore", "explore", false, null, true, ::ExplorePage)
 val showPage = Page("Show", "show", false, null, false, ::ShowPage, ShowPage::canExit)
 val subscribePage = Page("Subscribe", "subscribe", true, null, false, ::SubscriptionPage)
-val testPage = Page("Test", "test", true, adminRole, true, ::TestPage)
 val administrationPage = Page("Administration", "administration", true, adminRole, true, ::AdministrationPage)
 
-val pages = listOf(homePage, explorePage, showPage, subscribePage, testPage, administrationPage)
+val pages = listOf(homePage, explorePage, showPage, subscribePage, administrationPage)
 

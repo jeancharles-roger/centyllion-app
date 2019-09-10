cc1826fb8a5d9eedbc8bc1b3c09ea9cdddc25661
Jean-Charles <jeancharles.roger@me.com>
2019-09-10 15:20:10 +0200

    Adds tags

--- src/jsMain/kotlin/com/centyllion/client/controller/navigation/GrainModelDisplayController.kt
+++ src/jsMain/kotlin/com/centyllion/client/controller/navigation/GrainModelDisplayController.kt
@@ -6,6 +6,8 @@ import bulma.Div
 import bulma.Help
 import bulma.Label
 import bulma.NoContextController
+import bulma.Tag
+import bulma.Tags
 import com.centyllion.client.markdownToHtml
 import com.centyllion.model.GrainModelDescription
 import kotlin.properties.Delegates.observable
@@ -14,26 +16,29 @@ class GrainModelDisplayController(modelDescription: GrainModelDescription) :
     NoContextController<GrainModelDescription, Card>() {
 
     override var data by observable(modelDescription) { _, old, new ->
-        if (old != new)  refresh()
+        if (old != new) {
+            refresh()
+        }
     }
 
     override var readOnly = false
 
     val name = Label(data.model.name)
     val description = Div().apply { root.innerHTML = markdownToHtml(data.model.description) }
-
+    val tags = Tags(createTags(modelDescription.tags))
     val author = Help(data.info.user?.name?.let {"by $it"} ?: "")
 
 
-    override val container = Card(
-        CardContent(name, description, author)
-    ).apply {
-        root.classList.add("is-outlined")
-    }
+    override val container = Card(CardContent(name, description, author), CardContent(tags))
 
     override fun refresh() {
         name.text = data.name
         description.root.innerHTML = markdownToHtml(data.model.description)
+        tags.tags = createTags(data.tags)
         author.text = data.info.user?.name?.let {"by $it"} ?: ""
     }
+
+    fun createTags(source: String) =
+        source.split(" ").filter { it.isNotBlank() }
+              .map { tag -> Tag(tag, rounded = true) }
 }

f57384bfb47219a745d074ee6a20154e515115fe
Jean-Charles <jeancharles.roger@me.com>
2019-10-15 22:59:25 +0200

    Completes the english version

--- src/jsMain/kotlin/com/centyllion/client/tutorial/BacteriasTutorial.kt
+++ src/jsMain/kotlin/com/centyllion/client/tutorial/BacteriasTutorial.kt
@@ -7,6 +7,7 @@ import bulma.Columns
 import bulma.Icon
 import bulma.Image
 import bulma.SubTitle
+import bulma.TextColor
 import bulma.p
 import bulma.span
 import bulma.wrap
@@ -15,7 +16,11 @@ import com.centyllion.client.controller.model.GrainEditController
 import com.centyllion.client.controller.model.Simulator3dViewController
 import com.centyllion.client.page.ShowPage
 import com.centyllion.model.Grain
+import kotlinx.html.a
+import kotlinx.html.i
 import kotlinx.html.li
+import kotlinx.html.p
+import kotlinx.html.span
 import kotlinx.html.ul
 
 class BacteriasTutorial(
@@ -29,7 +34,7 @@ class BacteriasTutorial(
     private var bacteriaGrain: Grain? = null
 
     override val introduction: List<BulmaElement> = listOf(
-        p(i18n("With this tutorial you will create a simulation bacterias division with only one grain and one behaviour")),
+        p(i18n("With this tutorial you will create a simulation bacterias division with only one grain and one behaviour.")),
         Columns(
             Column(Image("https://centyllion.com/assets/images/bacterias-tutorial.jpg"), size = ColumnSize.Half),
             centered = true
@@ -38,7 +43,12 @@ class BacteriasTutorial(
 
     override val steps: List<TutorialStep> = listOf(
         TutorialStep(
-            i18n("Create a bacteria grain"), listOf(span(i18n("Adds a grain to the simulation"))),
+            i18n("Create a bacteria grain"),
+            listOf(
+                span(i18n("Click on ")),
+                Icon("plus", color = TextColor.Primary),
+                span(i18n(" to add a grain to the simulation."))
+            ),
             { page.modelController.addGrainButton.root },
             { page.model.model.grains.isNotEmpty() },
             {
@@ -47,7 +57,7 @@ class BacteriasTutorial(
             }
         ),
         TutorialStep(
-            i18n("Change the name"), listOf(span(i18n("You can change the grain name, for instance to 'bact'"))),
+            i18n("Change the name"), listOf(span(i18n("You can change the grain name, for instance to 'bact'."))),
             {
                 val editor = page.modelController.editor
                 if (editor is GrainEditController) editor.nameController.root
@@ -57,43 +67,48 @@ class BacteriasTutorial(
             { bacteriaGrain = page.model.model.grains.first() }
         ),
         TutorialStep(
-            i18n("Go to simulation"), listOf(span(i18n("Open the simulation page"))),
+            i18n("Go to simulation"), listOf(span(i18n("Open the simulation page, to test the model."))),
             { page.simulationPage.title.root },
             { page.editionTab.selectedPage == page.simulationPage }
         ),
         TutorialStep(
             i18n("Draw some grains"),
-            listOf(span(i18n("Select the ")), Icon(Simulator3dViewController.EditTools.Pen.icon), span(i18n(" tool"))),
+            listOf(span(i18n("Select the ")), Icon(Simulator3dViewController.EditTools.Pen.icon), span(i18n(" tool."))),
             { page.simulationController.simulationViewController.toolButtons[1].root },
             { page.simulationController.simulationViewController.tool == Simulator3dViewController.EditTools.Pen }
         ),
         TutorialStep(
-            i18n("Draw some grains"), listOf(span(i18n("Draw about %0 bacterias", bacteriaDraw + 5))),
+            i18n("Draw some grains"), listOf(span(i18n("Draw about %0 bacterias.", bacteriaDraw + 5))),
             { page.simulationController.simulationViewController.simulationCanvas.root },
             { page.simulationController.simulator.grainsCounts()[bacteriaGrain?.id ?: 0] ?: 0 > bacteriaDraw }
         ),
         TutorialStep(
-            i18n("Run the simulation"), listOf(span(i18n("Watch the bacterias move"))),
+            i18n("Run the simulation"), listOf(span(i18n("Watch the bacterias move."))),
             { page.simulationController.runButton.root },
             { page.simulationController.simulator.step > 5 }
         ),
         TutorialStep(
-            i18n("Stop the simulation"), listOf(span(i18n("Let's add a behaviour for bacteria division"))),
+            i18n("Stop the simulation"), listOf(span(i18n("Ok, the bacterias moves."))),
             { page.simulationController.stopButton.root },
             { !page.simulationController.isRunning }
         ),
         TutorialStep(
-            i18n("Go to model"), listOf(span(i18n("Open the model page to add a behaviour"))),
+            i18n("Go to model"), listOf(span(i18n("Open the model page to add a division behaviour to make them grow."))),
             { page.modelPage.title.root },
             { page.editionTab.selectedPage == page.modelPage }
         ),
         TutorialStep(
-            i18n("Create a division behaviour"), listOf(span(i18n("Add a behaviour to the simulation"))),
+            i18n("Create a division behaviour"),
+            listOf(
+                span(i18n("Click on ")),
+                Icon("plus", color = TextColor.Primary),
+                span(i18n(" to add a behaviour to the simulation."))
+            ),
             { page.modelController.addBehaviourButton.root },
             { page.model.model.behaviours.isNotEmpty() }
         ),
         TutorialStep(
-            i18n("First product"), listOf(span(i18n("Select the bacteria grain as first product"))),
+            i18n("First product"), listOf(span(i18n("Select the bacteria grain as first product."))),
             {
                 val editor = page.modelController.editor
                 if (editor is BehaviourEditController) editor.mainProductController.root
@@ -102,7 +117,12 @@ class BacteriasTutorial(
             { page.model.model.behaviours.firstOrNull()?.mainProductId == bacteriaGrain?.id }
         ),
         TutorialStep(
-            i18n("Adds a second product"), listOf(span(i18n("Add a second line in the behaviour"))),
+            i18n("Adds a second product"),
+            listOf(
+                span(i18n("Click on ")),
+                Icon("plus", color = TextColor.Info),
+                span(i18n(" to add a second line in the behaviour"))
+            ),
             {
                 val editor = page.modelController.editor
                 if (editor is BehaviourEditController) editor.addReactionButton.root
@@ -111,7 +131,7 @@ class BacteriasTutorial(
             { (page.model.model.behaviours.firstOrNull()?.reaction?.size ?: 0) > 0 }
         ),
         TutorialStep(
-            i18n("Second product"), listOf(span(i18n("Select the bacteria grain as second product"))),
+            i18n("Second product"), listOf(span(i18n("Select the bacteria grain as second product."))),
             {
                 page.modelController.editor?.let {
                     if (it is BehaviourEditController)
@@ -125,26 +145,33 @@ class BacteriasTutorial(
             }
         ),
         TutorialStep(
-            i18n("Return to simulation"), listOf(span(i18n("Open the simulation page"))),
+            i18n("Return to simulation"), listOf(span(i18n("Go back to the simulation page."))),
             { page.simulationPage.title.root },
             { page.editionTab.selectedPage == page.simulationPage }
         ),
         TutorialStep(
-            i18n("Run the simulation"), listOf(span(i18n("Watch the bacteria colony grow"))),
+            i18n("Run the simulation"), listOf(span(i18n("Watch the bacteria colony grow."))),
             { page.simulationController.runButton.root },
-            { page.simulationController.simulator.step > 30 },
+            { page.simulationController.simulator.step > 20 },
             { page.simulationController.stop() }
         )
     )
 
     override val conclusion: List<BulmaElement> = listOf(
         SubTitle(i18n("You've just created a simulation with Centyllion, well done 👍.")),
-        p(i18n("You can now for instance:")),
         wrap("content") {
+            p { +i18n("You can now for instance:") }
             ul {
                 li { +i18n("Set a half-life for bacterias to give them a life-span.") }
                 li { +i18n("Add a sugar grain to feed the bacterias.") }
                 li { +i18n("Create another bacteria to compete with.") }
+            }
+            p {
+                +i18n("You can find some documentation here ")
+                a("https://centyllion.com/fr/documentation.html") {
+                    span("icon is-primary") { i("fas fa-question-circle") }
+                }
+                + "."
             }
         }
     )

18d73d3bf89705b8f35ec60eb1d302c47289f2de
Jean-Charles <jeancharles.roger@me.com>
2019-10-15 15:38:13 +0200

    Updates target platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,8 +108,7 @@ kotlin {
 
             compilations.forEach {
                 it.kotlinOptions {
-                    jvmTarget = "1.8"
-
+                    jvmTarget = "11"
                 }
             }
         }

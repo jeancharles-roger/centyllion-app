86f84b5c34a16a03d62ed542aab3c32f4febb3c5
Jean-Charles <jeancharles.roger@me.com>
2019-10-31 11:34:46 +0100

    Adds resize and simplifies uPlot

--- src/jsMain/kotlin/com/centyllion/client/controller/model/ChartController.kt
+++ src/jsMain/kotlin/com/centyllion/client/controller/model/ChartController.kt
@@ -3,13 +3,21 @@ package com.centyllion.client.controller.model
 import bulma.Div
 import bulma.NoContextController
 import com.centyllion.client.controller.utils.push
-import uplot.AxeOption
-import uplot.ScaleOptions
-import uplot.SerieOptions
-import uplot.UPlotOptions
-import uplot.uPlot
+import org.w3c.dom.HTMLElement
+import kotlin.js.Json
 import kotlin.js.json
 import kotlin.properties.Delegates.observable
+
+
+@JsName("uPlot")
+private external class uPlot(options: Json, data: Array<Array<out Number>>) {
+
+    val root: HTMLElement
+
+    fun getView(): Array<Number>
+
+    fun setData(data: Array<Array<out Number>>, minView: Number = definedExternally, maxView: Number = definedExternally)
+}
 
 
 data class ChartLine(
@@ -27,9 +35,8 @@ class ChartData(
 )
 
 class ChartController(
-    chart: Chart
+    chart: Chart, size: Pair<Int, Int> = 800 to 400
 ): NoContextController<Chart, Div>() {
-
 
     private var chartData = ChartData(1, Array(chart.lines.size) { emptyArray<Int>() })
 
@@ -41,9 +48,16 @@ class ChartController(
 
     private var yMax = yValues.map { it.max() ?: 0 }.max() ?: 0
 
-    private var uplot: uPlot? by observable(createUPlot(chart)) { _, old, new ->
+    private var uplot: uPlot? by observable(createUPlot(chart, size.first, size.second)) { _, old, new ->
         if (old != null) container.root.removeChild(old.root)
         if (new != null) container.root.appendChild(new.root)
+    }
+
+    var size: Pair<Int, Int> by observable(size) { _, old, new ->
+        if (old != new) {
+            // recreate uPlot
+            uplot = createUPlot(data, new.first, new.second)
+        }
     }
 
     override var data: Chart by observable(chart) { _, old, new ->
@@ -53,7 +67,7 @@ class ChartController(
             reset()
 
             // recreate uPlot
-            uplot = createUPlot(new)
+            uplot = createUPlot(new, size.first, size.second)
         }
     }
 
@@ -68,30 +82,29 @@ class ChartController(
     }
 
     private fun graphRangeX(min: Number, max: Number): Array<Number> {
-        return arrayOf(0, xMax)
+        return arrayOf(min, max)
     }
 
     private fun graphRangeY(min: Number, max: Number): Array<Number> {
         return arrayOf(0, yMax + (0.1 * yMax))
     }
 
-    private fun createUPlot(chart: Chart): uPlot? {
+    private fun createUPlot(chart: Chart, width: Int = 800, height: Int = 400): uPlot? {
         if (chart.lines.isEmpty()) return null
         return uPlot(
-            UPlotOptions(800, 400).apply {
-                scales = json(
-                    "x" to ScaleOptions(type = "n", range = ::graphRangeX),
-                    "y" to ScaleOptions(type = "n", range = ::graphRangeY)
+            json(
+                "width" to width, "height" to height,
+                "scales" to json(
+                    "x" to json("type" to "n", "range" to ::graphRangeX),
+                    "y" to json("type" to "n", "range" to ::graphRangeY)
+                ),
+                "series" to json(
+                    "x" to json("label" to data.xLabel),
+                    "y" to chart.lines.map {
+                        json("type" to "n", "label" to it.label, "color" to it.color, "width" to it.width)
+                    }.toTypedArray()
                 )
-                axes.y = arrayOf(AxeOption(scale = "y"))
-                series.x.label = data.xLabel
-                series.y = chart.lines.map {
-                    SerieOptions(
-                        scale = "y", label = it.label, color = it.color,
-                        width = it.width
-                    )
-                }.toTypedArray()
-            },
+            ),
             arrayOf(xValues, *yValues)
         )
     }
